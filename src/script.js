d2f735470e944f63b0c9b257db33f8b047ebaaa0
Ricardo <ricardosousa339@gmail.com>
2022-03-12 00:06:28 -0300

    Implementacao de funcao de adicionar filme

--- src/script.js
+++ src/script.js
@@ -1,9 +1,17 @@
-var listaFilmes = [
-  "https://i.ytimg.com/vi/uaSYEUugnzE/movieposter_en.jpg",
-  "https://s2.glbimg.com/fCNS_fXPhGRwlpnAaQLzvGYC1y0=/362x536/https://s2.glbimg.com/UPI9xlM9R9O41YRqSO7R3hTNk_s=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/q/V/Avo9ISQQO3grnARi3JCA/2020-914-a-chegada-poster.jpg",
-  "https://br.web.img2.acsta.net/c_310_420/medias/nmedia/18/96/78/04/20488762.jpg"
-];
+function adicionarFilme() {
+  var campoFilmeFavorito = document.getElementById("filme").value;
 
-for (var i = 0; i < listaFilmes.length; i++) {
-  document.write("<img src=" + listaFilmes[i] + ">");
+  if (campoFilmeFavorito.endsWith(".jpg")) {
+    listarFilmesNaTela(campoFilmeFavorito);
+  } else {
+    console.error("Endereço de filme inválido");
+  }
+  document.getElementById("filme").value = "";
+}
+
+function listarFilmesNaTela(filme) {
+  var elementoFilmeFavorito = "<img src=" + filme + ">";
+  var elementoListaFilmes = document.getElementById("listaFilmes");
+  elementoListaFilmes.innerHTML =
+    elementoFilmeFavorito + elementoListaFilmes.innerHTML;
 }
